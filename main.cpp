87c32cb3c584beb1d32a53a98967aceb94013afd
adam1557 <97037671+adam1557@users.noreply.github.com>
2022-09-10 16:26:18 +0200

    Update main.cpp
    A hibák kommentelése.

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; // NELEMENTS helyet N_ELEMENTS a használt változó név
+    std::cout << '1-100 ertekek duplazasa' //'' helyett "" kell mivel ez nem egy karakter, hanem sztring, valamint a végéről hiányzik a pontosvessző. +sortörés szükséges
+    for (int i = 0;)// A for ciklus szintaktikailag nem teljes
     {
-        b[i] = i * 2;
+        b[i] = i * 2; //pontosvessző hiánya. +sortörés szükséges
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) //nem szükséges még egy for ciklus, a kiírás megoldható az előző for ciklusban
     {
         std::cout << "Ertek:"
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag;//"atlag" változó inicializálása hiányzik
+    for (int i = 0; i < N_ELEMENTS, i++) // vessző helyett pontosvessző kell a for ciklus belsejében(szintaktika végett), valamint az egyenlőséget is megengedjük.
     {
-        atlag += b[i]
+        atlag += b[i] // pontosvessző hiánya
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
